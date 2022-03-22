16c33a500c7aa531d6618d5118a7a44ce5371b7a
SohybBinselm <33469005+SohybBinselm@users.noreply.github.com>
2022-03-22 12:39:47 +0200

    Update style.dart

--- lib/extra/style.dart
+++ lib/extra/style.dart
@@ -2,20 +2,26 @@ import 'package:date_picker_timeline/extra/color.dart';
 import 'package:flutter/material.dart';
 import 'package:date_picker_timeline/extra/dimen.dart';
 
-const TextStyle defaultMonthTextStyle = TextStyle(
-  color: AppColors.defaultMonthColor,
-  fontSize: Dimen.monthTextSize,
-  fontWeight: FontWeight.w500,
+const TextStyle defaultMonthTextStyle =TextStyle(
+  color: Color(0xDE000000),
+  letterSpacing: .22,
+  fontSize: 12,
+  fontFamily: "Nunito",
+  fontWeight: FontWeight.w600,
 );
 
-const TextStyle defaultDateTextStyle = TextStyle(
-  color: AppColors.defaultDateColor,
-  fontSize: Dimen.dateTextSize,
-  fontWeight: FontWeight.w500,
+const TextStyle defaultDateTextStyle =TextStyle(
+  color: Color(0xDE000000),
+  letterSpacing: .25,
+  fontSize: 14,
+  fontFamily: "Nunito",
+  fontWeight: FontWeight.w600,
 );
 
-const TextStyle defaultDayTextStyle = TextStyle(
-  color: AppColors.defaultDayColor,
-  fontSize: Dimen.dayTextSize,
-  fontWeight: FontWeight.w500,
+const TextStyle defaultDayTextStyle =TextStyle(
+  color: Color(0xDE000000),
+  letterSpacing: .22,
+  fontSize: 12,
+  fontFamily: "Nunito",
+  fontWeight: FontWeight.w600,
 );
